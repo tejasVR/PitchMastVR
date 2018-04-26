49ad2409c324f6e4c00217172b8d700e2c8c9a33
Tejas Shroff <tejasshroff24@gmail.com>
2018-04-26 14:05:14 -0400

    Creating sample scrool UI script
    I am modeling how we can scroll through a variety of elements in VR using the Vive controllers

---
+++ Assets/0_Project/Scripts/PopulateGridTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopulateGridTest : MonoBehaviour {
+
+    public GameObject cubePrefab;
+
+    public int numberToCreate;
+
+	// Use this for initialization
+	void Start () {
+
+        Populate();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Populate()
+    {
+        GameObject newObj;
+
+        for (int i = 0; i < numberToCreate; i++)
+        {
+            newObj = (GameObject)Instantiate(cubePrefab, transform);
+            newObj.GetComponent<Renderer>().material.color = Random.ColorHSV();
+
+
+        }
+    }
+}
